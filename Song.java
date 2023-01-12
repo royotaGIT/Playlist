3ba4f9375f6095d0adfb8b9270d7d231a6d1f247
Roy Otamura <rotamura24@students.polytechnic.org>
2023-01-12 09:11:17 -0800

    methods done, song class done,

--- Song.java
+++ Song.java
@@ -2,10 +2,12 @@ public class Song{
     String title;
     String artist;
     String duration;
-    boolean liked;
+    boolean liked = false;
     public Song(String myTitle, String myArtist, String myDuration){title=myTitle;artist=myArtist;duration=myDuration;}
     public String getTitle(){return title;}
     public String getArtist(){return artist;}
     public String getDuration(){return duration;}
     public boolean isLiked(){return liked;}
+    public void like(){liked = true;}
+    public void unLike(){liked = false;}
 }
