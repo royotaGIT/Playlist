034bfcbf8c6402461a6c707fff53ba61a628ea0c
Roy Otamura <rotamura24@students.polytechnic.org>
2023-01-12 09:09:15 -0800

    fields initialized, getters created

--- Song.java
+++ Song.java
@@ -1,3 +1,11 @@
 public class Song{
-    
+    String title;
+    String artist;
+    String duration;
+    boolean liked;
+    public Song(String myTitle, String myArtist, String myDuration){title=myTitle;artist=myArtist;duration=myDuration;}
+    public String getTitle(){return title;}
+    public String getArtist(){return artist;}
+    public String getDuration(){return duration;}
+    public boolean isLiked(){return liked;}
 }
