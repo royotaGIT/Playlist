759c45a889ccb3b3571424a5e227f011b24f0b05
Roy Otamura <rotamura24@students.polytechnic.org>
2023-01-12 09:23:59 -0800

    created playlist tester

---
+++ playlistTester.java
@@ -0,0 +1,6 @@
+public class playlistTester {
+    public static void main(String[] args){
+        System.out.println("Initializing playlist...");
+        Playlist bops = new Playlist();
+    }
+}
